83ce2931eba56a7d2d4c4133a3350d5c761f52df
Ivana Jindrová <ivajindrova@gmail.com>
2024-02-15 20:17:08 +0100

    Uložení úkolu01

---
+++ script.js
@@ -0,0 +1,8 @@
+const stupen = Number(prompt("Zadej teplotu ve stupních Fahrenheita: "))
+
+let citatel = 5 * (stupen - 32)
+let stCelsia = citatel / 9
+
+console.log(stCelsia)
+
+document.body.innerHTML = "<p> Na troubě nastav " + Math.round(stCelsia) + " stupňů Celsia.</p>"
